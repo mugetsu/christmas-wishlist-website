bd55df25bf30281c85831d0055ed8320d6c4f5c2
randell <cprjk.buzz@gmail.com>
2026-09-03 20:02:10 +0800

    update: Added simple wishlist website

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Replace these two values with your Supabase project values.
 // Project Settings → API
 export const SUPABASE_URL = "https://njebnzxifizltqpylfiz.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_wlsLq5ybDyAGl90wRudr-w_3KNC2Ba9";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZWJuenhpZml6bHRxcHlsZml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjY1MjEsImV4cCI6MjEwNDAwMjUyMX0.DFw_LsUFpblRDH1QVjuEmlrsDJwRSqSBHXLxdi9rKHE";
